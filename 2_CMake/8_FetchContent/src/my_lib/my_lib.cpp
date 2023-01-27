e68b273793e6312a5a896cd98572d2ec5d357bab
thatgeeman <thatgeeman@users.noreply.github.com>
2023-01-27 15:24:05 +0100

    Add 8_FetchContent

---
+++ 2_CMake/8_FetchContent/src/my_lib/my_lib.cpp
@@ -0,0 +1,12 @@
+#include "my_lib.h"
+#include <fmt/format.h>
+#include <iostream>
+#include <nlohmann/json.hpp>
+
+void print_hello_world()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "JSON Lib version (from my_lib.cpp):" << NLOHMANN_JSON_VERSION_MAJOR << "."
+              << NLOHMANN_JSON_VERSION_MINOR << "." << NLOHMANN_JSON_VERSION_PATCH << "\n";
+    std::cout << "FMT Lib version:" << FMT_VERSION << "\n";
+}
